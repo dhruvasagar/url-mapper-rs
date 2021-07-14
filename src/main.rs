e1455775c6d560c53270acbeb5760919368dc82c
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-07-14 22:16:58 +0530

    Part IX - Handling Signals & Deploying to Kubernetes

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use crate::db::{DB, Manager};
 use tracing::subscriber::set_global_default;
 use tracing_subscriber::FmtSubscriber;
 use server::Server;
+use std::process;
 
 mod config;
 mod db;
@@ -18,6 +19,23 @@ async fn main() -> Result<()> {
     tokio::spawn(async move {
         let mut manager = Manager::new(db, db_rx);
         manager.listen().await;
+    });
+
+    tokio::spawn(async move {
+        use tokio::signal::unix::{signal, SignalKind};
+        let mut hup = signal(SignalKind::hangup()).unwrap();
+        let mut int = signal(SignalKind::interrupt()).unwrap();
+        let mut quit = signal(SignalKind::quit()).unwrap();
+        let mut term = signal(SignalKind::terminate()).unwrap();
+
+        tokio::select! {
+            _ = hup.recv() => tracing::info!("Recieved SIGHUP!"),
+            _ = int.recv() => tracing::info!("Recieved SIGINT!"),
+            _ = quit.recv() => tracing::info!("Recieved SIGQUIT!"),
+            _ = term.recv() => tracing::info!("Recieved SIGTERM!"),
+        }
+        tracing::info!("Good Bye from Url Mapper in Rust!");
+        process::exit(0);
     });
 
     Server::new(db_tx).listen().await?;
